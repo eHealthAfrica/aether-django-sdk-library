f695776e940a8fa1fc8cb9527851849148006e59
obdulia <obdulia.losantos@gmail.com>
2019-04-29 09:52:25 +0200

    feat: bring setup from aether.common

---
+++ entrypoint.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+set -Eeuo pipefail
+
+function show_help {
+    echo """
+    Commands
+    ----------------------------------------------------------------------------
+    bash          : run bash
+    eval          : eval shell command
+    manage        : invoke django manage.py commands
+
+    test          : run tests
+    test_lint     : run flake8 tests
+    test_coverage : run tests with coverage output
+    test_py       : alias of test_coverage
+
+    build         : build package library
+    """
+}
+
+function test_flake8 {
+    flake8
+}
+
+function test_coverage {
+    rm -R /code/.coverage* 2>/dev/null || true
+
+    coverage run \
+        --concurrency=multiprocessing \
+        --parallel-mode \
+        manage.py test --parallel --noinput "${@:1}"
+
+    coverage combine --append
+    coverage report
+    coverage erase
+}
+
+case "$1" in
+    bash )
+        bash
+    ;;
+
+    eval )
+        eval "${@:2}"
+    ;;
+
+    manage )
+        python manage.py "${@:2}"
+    ;;
+
+    test )
+        export TESTING=true
+        test_flake8
+        test_coverage "${@:2}"
+    ;;
+
+    test_lint )
+        export TESTING=true
+        test_flake8
+    ;;
+
+    test_py | test_coverage )
+        export TESTING=true
+        test_coverage "${@:2}"
+    ;;
+
+    build )
+        # remove previous packages if needed
+        rm -rf dist/*
+        rm -rf build
+        rm -rf django_eha_sdk.egg-info
+
+        # create the distribution
+        python setup.py bdist_wheel
+
+        # remove useless content
+        rm -rf build
+        rm -rf django_eha_sdk.egg-info
+    ;;
+
+    help )
+        show_help
+    ;;
+
+    *)
+        show_help
+    ;;
+esac
---
+++ setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2019 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from io import open
+from setuptools import find_packages, setup
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+setup(
+    version='0.0.1',
+    name='django_eha_sdk',
+    description='A python library with helpful django tools',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/eHealthAfrica/django-eha-sdk/',
+    author='eHealth Africa',
+    author_email='info@ehealthafrica.org',
+    license='Apache2 License',
+
+    python_requires='>=3.6',
+    install_requires=[
+        'django>=2',
+        'django-cors-headers',
+        'django-debug-toolbar',
+        'django-prometheus',
+        'django-uwsgi',
+        'djangorestframework>=3.8',
+        'drf-dynamic-fields',
+        'psycopg2-binary',
+        'pygments',
+        'python-json-logger',
+        'requests',
+        'urllib3>=1.25',
+    ],
+    extras_require={
+        'cas': [
+            'django-cas-ng>=3.6',
+        ],
+        'storage': [
+            'django-minio-storage',
+            'django-storages[boto3,google]',
+        ],
+        'server': [
+            'sentry-sdk',
+            'uwsgi',
+        ],
+        'webpack': [
+            'django-webpack-loader',
+        ],
+        'scheduler': [
+            'django-rq',
+            'redis',
+            'rq',
+            'rq-scheduler',
+        ],
+        'test': [
+            'coverage',
+            'flake8',
+            'flake8-quotes',
+            'tblib',  # for paralell test runner
+        ],
+    },
+
+    packages=find_packages(exclude=['*tests*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apple Public Source License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+)
