994e58da976a3a4eee3c1bd48f7c14793eddc325
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2019-08-16 14:26:08 +0200

    fix(auth): check user in realm also for TokenAuth (#19)
    * refactor: rename GatewayBasicAuthentication to BasicAuthentication
    
    * fix(auth): check user in realm also for TokenAuth

--- aether/sdk/conf/settings.py
+++ aether/sdk/conf/settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'aether.sdk.auth.authentication.BasicAuthentication',
+        'aether.sdk.auth.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
